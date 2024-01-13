63964e78e6ebf4e328639f53088b3e880190ab88
Andreea-Lita <litaandreeamalaiasi@gmail.com>
2024-01-13 21:01:21 +0000

    Added a timer functionality, erased the optional commended one

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,26 +1,3 @@
-//var timer = document.querySelector('#timer');
-//var counter = 60
-//setInterval(function() {
-//  counter = counter - 1;
-// console.log('counter')
-//}, 1000)
-/* const startButton = document.getElementById("start");
-function startTimer(duration = 6, displayElementId = "time") {
-    let secondsRemaining = duration;
-    const timerId = setInterval(() => {
-        const minutes = Math.floor(secondsRemaining / 6);
-        const seconds = secondsRemaining % 6;
-        document.getElementById(displayElementId).textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-        secondsRemaining--;
-        if (secondsRemaining === 0) clearInterval(timerId);
-    }, 1000);
-}
-startTimer();
-
-startButton.addEventListener("click", () => {
-    startTimer();
-    startButton.disabled = true; // Disable the button to prevent multiple starts
-});*/
 var startButton = document.getElementById("start");
 var durationRemaining = 6;
 var displayElementId = document.getElementById("time");
